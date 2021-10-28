42cdefafc074b4cfd3e7b908927ad28f29df66b7
Wendel-Ernandes <wendel.opbr@gmail.com>
2021-10-28 02:26:28 -0300

    CRUD fixed 1

--- app/Controllers/Http/EventsController.ts
+++ app/Controllers/Http/EventsController.ts
@@ -4,56 +4,60 @@ import StoreValidator from 'App/Validators/Event/StoreValidator'
 import UpdateValidator from 'App/Validators/Event/UpdateValidator'
 
 export default class EventsController {
-  public async index(ctx: HttpContextContract) {
+  public async index({ view }: HttpContextContract) {
     const events = await Event.query()
-    return ctx.view.render('events/list', {
+    return view.render('events/list', {
       events,
     })
   }
 
-  public async create(ctx: HttpContextContract) {
-    return ctx.view.render('events/create')
+  public async create({ view }: HttpContextContract) {
+    return view.render('events/create')
   }
 
-  public async store(ctx: HttpContextContract) {
-    const data = await ctx.request.validate(StoreValidator)
+  public async store({ response, request }: HttpContextContract) {
+    const data = await request.validate(StoreValidator)
     await Event.create(data)
-    return ctx.response.redirect('/events')
+    return response.redirect('/events')
   }
 
-  public async edit(ctx: HttpContextContract) {
-    const event = await Event.find(ctx.params.id)
+  public async edit({ response, view, params }: HttpContextContract) {
+    const event = await Event.find(params.id)
     if (!event) {
-      return ctx.response.notFound('Event Not Found')
+      return response.notFound('Event Not Found')
     }
-    return ctx.view.render('events/edit', { event })
+
+    return view.render('events/edit', { event })
   }
 
-  public async update(ctx: HttpContextContract) {
-    const event = await Event.find(ctx.params.id)
+  public async update({ params, response, request }: HttpContextContract) {
+    const event = await Event.find(params.id)
     if (!event) {
-      return ctx.response.notFound('Event Not Found')
+      return response.notFound('Event Not Found')
     }
-    const data = await ctx.request.validate(UpdateValidator)
+
+    const data = await request.validate(UpdateValidator)
     event.merge(data)
     await event.save()
-    return ctx.response.redirect('/events')
+    return response.redirect('/events')
   }
 
-  public async show(ctx: HttpContextContract) {
-    const event = await Event.find(ctx.params.id)
+  public async show({ response, view, params }: HttpContextContract) {
+    const event = await Event.find(params.id)
     if (!event) {
-      return ctx.response.notFound('Event Not Found')
+      return response.notFound('Event Not Found')
     }
-    return ctx.view.render('events/index', { event })
+
+    return view.render('events/index', { event })
   }
 
-  public async destroy(ctx: HttpContextContract) {
-    const event = await Event.find(ctx.params.id)
+  public async destroy({ response, params }: HttpContextContract) {
+    const event = await Event.find(params.id)
     if (!event) {
-      return ctx.response.notFound('Event Not Found')
+      return response.notFound('Event Not Found')
     }
+
     await event.delete()
-    return ctx.response.redirect('/events')
+    return response.redirect('/events')
   }
 }
