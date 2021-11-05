1a7267108a7cc5b488bbe495d6e728f0bac3bbdc
Manuel-Antunes <nerdbroz2@gmail.com>
2021-11-05 01:33:58 -0300

    fix: send assets to resource page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'dungeons-and-dragons': "url('../../public/assets/img/dungeons-and-dragons.jpg')",
-        'vampire-the-mascarade': "url('../../public/assets/img/vampire-the-mascarade.jpeg')",
-        'al-rpg-club': "url('../../public/assets/img/al-rpg-club.jpg')",
-        'cyberpunk': "url('../../public/assets/img/cyberpunk.jpg')",
-        'logo': "url('../../public/assets/img/pp.jpg')",
-        'tabuleiro-rpg': "url('../../public/assets/img/tabuleiro-rpg.jpg')",
+        'dungeons-and-dragons': "url('../images/dungeons-and-dragons.jpg')",
+        'vampire-the-mascarade': "url('../images/vampire-the-mascarade.jpeg')",
+        'al-rpg-club': "url('../images/al-rpg-club.jpg')",
+        'cyberpunk': "url('../images/cyberpunk.jpg')",
+        'logo': "url('../images/pp.jpg')",
+        'tabuleiro-rpg': "url('../images/tabuleiro-rpg.jpg')",
       },
       blur: {
         sm: '2px',
