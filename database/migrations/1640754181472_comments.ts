73e168e7612e2e7cf48cc81c2af2c8e0d55a800f
Eduardo <eduardoldq1@gmail.com>
2021-12-29 02:15:25 -0300

    feat: add comment migration

---
+++ database/migrations/1640754181472_comments.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Comments extends BaseSchema {
+  protected tableName = 'comments'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('message').notNullable()
+      table.integer('user_id').notNullable().references('id').inTable('users')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
