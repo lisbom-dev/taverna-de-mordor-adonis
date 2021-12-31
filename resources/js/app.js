4305a3f9c5880cdee3ac9a3e41455bcdc3f4831e
Eduardo <eduardoldq1@gmail.com>
2021-12-30 21:08:30 -0300

    chore: add alert component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,12 @@ import '../css/app.css'
 import Alpine from 'alpinejs'
 import axios from 'axios'
 import navbar from './partials/navbar'
+import alert from './components/alert'
 
 console.log(Alpine)
 
 Alpine.data('navbar', navbar)
+Alpine.data('alert', alert)
 
 window.Alpine = Alpine
 
