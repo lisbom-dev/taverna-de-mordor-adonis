22cfb66c506770ccc1e5564f73233ad20f8f0615
Eduardo <eduardoldq1@gmail.com>
2021-12-30 23:26:42 -0300

    feat: add star rating controllers

---
+++ app/Controllers/Http/BoardStarRatingsController.ts
@@ -0,0 +1,15 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Board from 'App/Models/Board'
+import StoreValidator from 'App/Validators/StarRating/StoreValidator'
+
+export default class BoardStarRatingsController {
+  public async store({ request, response, params }: HttpContextContract) {
+    const board = await Board.find(params.id)
+    if (!board) {
+      return response.notFound('Board not found!')
+    }
+    const data = await request.validate(StoreValidator)
+    await board.related('starRating').create(data)
+  }
+}
