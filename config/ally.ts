bd3b3cf8beee5025cb911d2fd75dd56b7c130b2e
= <=>
2021-12-30 23:31:50 -0300

    fix: use host with google callback

--- config/ally.ts
+++ config/ally.ts
@@ -27,7 +27,7 @@ const allyConfig: AllyConfig = {
     driver: 'google',
     clientId: Env.get('GOOGLE_CLIENT_ID'),
     clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
-    callbackUrl: '/google/callback',
+    callbackUrl: Env.get('HOST') + '/google/callback',
   },
 }
 
