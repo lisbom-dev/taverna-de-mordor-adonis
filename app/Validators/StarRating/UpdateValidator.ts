906d974bae3268268e0c100b4f90ab4e9212f6e8
Eduardo <eduardoldq1@gmail.com>
2021-12-31 10:39:46 -0300

    chore: add range rule

--- app/Validators/StarRating/UpdateValidator.ts
+++ app/Validators/StarRating/UpdateValidator.ts
@@ -1,12 +1,16 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UpdateValidator {
-  constructor(protected ctx: HttpContextContract) {}
+  constructor(protected ctx: HttpContextContract) {
+    return
+  }
 
   public schema = schema.create({
-    number: schema.number.optional([]),
+    number: schema.number.optional([rules.range(1, 5)]),
   })
 
-  public messages = {}
+  public messages = {
+    'number.range': 'Só é possível avaliar entre 1 e 5 estrelas!',
+  }
 }
