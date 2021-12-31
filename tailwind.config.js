ce81218c4de4c106967c6473ad616c1e3e0943ce
Manuel-Antunes <nerdbroz2@gmail.com>
2021-12-31 13:31:49 -0300

    feat: improve toasts

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,23 @@ module.exports = {
       blur: {
         sm: '2px',
       },
+      keyframes: {
+        'slide-from-right': {
+          '0%': {
+            'transform': 'translateX(100%)',
+            'opacity': '0.2',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '100%': {
+            'transform': 'translateX(0)',
+            'opacity': '1',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+      },
+      animation: {
+        'slide-from-right': 'slide-from-right 0.5s ease-in-out',
+      },
     },
   },
   variants: {
