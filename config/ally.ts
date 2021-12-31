b5dd1f4733b62905e5b926e7f976ebd569c824b0
= <=>
2021-12-30 23:21:59 -0300

    fix: google callback error

--- config/ally.ts
+++ config/ally.ts
@@ -27,7 +27,7 @@ const allyConfig: AllyConfig = {
     driver: 'google',
     clientId: Env.get('GOOGLE_CLIENT_ID'),
     clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
-    callbackUrl: 'http://localhost:3333/google/callback',
+    callbackUrl: '/google/callback',
   },
 }
 
