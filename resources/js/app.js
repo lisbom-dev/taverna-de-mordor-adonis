d5a3efa9263c6be225192f510ab537bfda9f51eb
Eduardo <eduardoldq1@gmail.com>
2022-01-01 19:22:38 -0300

    feat: install IMask

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,12 @@ import axios from 'axios'
 import navbar from './partials/navbar'
 import alert from './components/alert'
 import calendar from './partials/calendar'
-
+import IMask from 'imask'
 Alpine.data('navbar', navbar)
 Alpine.data('alert', alert)
 Alpine.data('calendar', calendar)
 
 window.Alpine = Alpine
-
+window.IMask = IMask
 Alpine.start()
 window.axios = axios.create({ baseURL: '/' })
