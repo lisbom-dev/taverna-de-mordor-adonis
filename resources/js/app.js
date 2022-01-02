3386c2df256480fcfde665c2f60a6bc81de36b98
Eduardo <eduardoldq1@gmail.com>
2022-01-02 01:38:10 -0300

    feat: add chat to alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,13 @@ import navbar from './partials/navbar'
 import alert from './components/alert'
 import calendar from './partials/calendar'
 import IMask from 'imask'
+import io from 'socket.io-client'
+import chat from './chat'
+
 Alpine.data('navbar', navbar)
 Alpine.data('alert', alert)
 Alpine.data('calendar', calendar)
-
+Alpine.data('chat', chat)
 window.Alpine = Alpine
 window.IMask = IMask
 Alpine.start()
