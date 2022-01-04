ab2ef1a6c5e876eb3a4f122de1142b1ed6f8d7de
Eduardo <eduardoldq1@gmail.com>
2022-01-04 00:02:23 -0300

    chore: add migration to relate system with boards

---
+++ database/migrations/1641264707396_modify_system_column_to_foreign_key_in_boards_tables.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Boards extends BaseSchema {
+  protected tableName = 'boards'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('system')
+      table
+        .integer('system_id')
+        .notNullable()
+        .references('id')
+        .inTable('systems')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('system_id')
+      table.string('system').notNullable()
+    })
+  }
+}
