2619e243cb507c6a8176d0bb6103f7927a9aab99
Eduardo <eduardoldq1@gmail.com>
2022-01-03 23:58:47 -0300

    feat: add model of system

---
+++ app/Models/System.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Board from './Board'
+
+export default class System extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public photoUrl: string
+
+  @column()
+  public oficialSite: string
+
+  @hasMany(() => Board)
+  public boards: HasMany<typeof Board>
+
+  @column()
+  public slug: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
