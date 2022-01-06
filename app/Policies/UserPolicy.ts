76d5037467a968ad801150b5f8126cb44576a5ef
Eduardo <eduardoldq1@gmail.com>
2022-01-06 14:01:58 -0300

    feat: add user policies

---
+++ app/Policies/UserPolicy.ts
@@ -0,0 +1,26 @@
+import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
+import Board from 'App/Models/Board'
+import User from 'App/Models/User'
+
+export default class UserPolicy extends BasePolicy {
+  public async invoke(user: User) {
+    return user.isAdm
+  }
+
+  public async view(userSeeing: User, userSeen: User, board: Board | null) {
+    if (userSeeing.isAdm) {
+      return true
+    }
+    if (userSeen.isMaster) {
+      return true
+    }
+    if (userSeeing.isMaster && board) {
+      var player = board.players.find((b) => b.id === userSeen.id)
+      if (!player) {
+        return false
+      }
+      return true
+    }
+    return false
+  }
+}
