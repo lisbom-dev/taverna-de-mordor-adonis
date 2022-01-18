35c32e2a0a3b7824acc7c219953029e1d851135e
Eduardo <eduardoldq1@gmail.com>
2022-01-18 12:06:07 -0300

    chore: add tailwind custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
       spacing: {
         110: '28rem',
       },
+      fontSize: {
+        '2xs': '10px',
+        '3xs': '9px',
+        '4xs': '5px',
+      },
       backgroundImage: {
         'dungeons-and-dragons': "url('../images/dungeons-and-dragons.jpg')",
         'vampire-the-mascarade': "url('../images/vampire-the-mascarade.jpeg')",
@@ -19,6 +24,8 @@ module.exports = {
         'logo': "url('../images/pp.jpg')",
         'tabuleiro-rpg': "url('../images/tabuleiro-rpg.jpg')",
         'main-cover': "url('../images/cover-rpg.jpg')",
+        'main-cover-gradient':
+          "linear-gradient(0.03deg, #111827, rgba(17, 24, 39, 0.9), rgba(17, 24, 39, 0.75), rgba(17, 24, 39, 0.4), rgba(17, 24, 39, 0.1), rgba(17, 24, 39, 0)),url('../images/cover-rpg.jpg')",
       },
       blur: {
         sm: '2px',
@@ -39,6 +46,27 @@ module.exports = {
       },
       animation: {
         'slide-from-right': 'slide-from-right 0.5s ease-in-out',
+      },
+      width: {
+        68: '280px',
+        83: '340px',
+        75: '300px',
+        100: '430px',
+        18: '67px',
+        130: '550px',
+        200: '700px',
+      },
+      height: {
+        30: '121px',
+        22: '88px',
+        66: '270px',
+        76: '295px',
+      },
+      left: {
+        75: '300px',
+      },
+      minHeight: {
+        60: '230px',
       },
     },
   },
