338c8068f93264cc82bf380932b15b2374927aff
Eduardo <eduardoldq1@gmail.com>
2022-01-18 13:32:36 -0300

    feat: add model of review

---
+++ app/Models/Review.ts
@@ -0,0 +1,73 @@
+import { DateTime } from 'luxon'
+import {
+  BaseModel,
+  beforeFetch,
+  beforeFind,
+  column,
+  hasOne,
+  HasOne,
+  manyToMany,
+  ManyToMany,
+  ModelQueryBuilderContract,
+} from '@ioc:Adonis/Lucid/Orm'
+import Board from './Board'
+import Event from './Event'
+import User from './User'
+
+export default class Review extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public rating: number
+
+  @column()
+  public comment: string
+
+  @column()
+  public senderId: number
+
+  @hasOne(() => User, {
+    foreignKey: 'sender_id',
+  })
+  public sender: HasOne<typeof User>
+
+  @manyToMany(() => Board, {
+    localKey: 'id',
+    pivotForeignKey: 'review_id',
+    relatedKey: 'id',
+    pivotRelatedForeignKey: 'board_id',
+    pivotTable: 'board_reviews',
+  })
+  public board: ManyToMany<typeof Board>
+
+  @manyToMany(() => Event, {
+    localKey: 'id',
+    pivotForeignKey: 'review_id',
+    relatedKey: 'id',
+    pivotRelatedForeignKey: 'event_id',
+    pivotTable: 'event_reviews',
+  })
+  public event: ManyToMany<typeof Event>
+
+  @manyToMany(() => User, {
+    localKey: 'id',
+    pivotForeignKey: 'review_id',
+    relatedKey: 'id',
+    pivotRelatedForeignKey: 'master_id',
+    pivotTable: 'master_reviews',
+  })
+  public master: ManyToMany<typeof User>
+
+  @beforeFetch()
+  @beforeFind()
+  public static preloadRelations(q: ModelQueryBuilderContract<typeof Review>) {
+    q.preload('board')
+  }
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
