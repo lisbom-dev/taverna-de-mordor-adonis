3e8bb22d73ebea9af5099076aad49ad2030a7fc8
Eduardo <eduardoldq1@gmail.com>
2022-01-28 23:09:48 -0300

    chore: implement board reviews controller

--- app/Controllers/Http/BoardReviewsController.ts
+++ app/Controllers/Http/BoardReviewsController.ts
@@ -3,12 +3,16 @@ import Board from 'App/Models/Board'
 import StoreValidator from 'App/Validators/Review/StoreValidator'
 
 export default class BoardReviewsController {
-  public async store({ request, params, response }: HttpContextContract) {
+  public async store({ request, params, response, bouncer, session }: HttpContextContract) {
     const board = await Board.find(params.board_id)
     if (!board) {
       return response.notFound('Board not found!')
     }
+    await board.load('reviews')
+    await bouncer.with('BoardReviewPolicy').authorize('create', board)
     const data = await request.validate(StoreValidator)
+    session.flash('success', ['Resenha criada com sucesso!'])
     await board.related('reviews').create(data)
+    return response.redirect().back()
   }
 }
