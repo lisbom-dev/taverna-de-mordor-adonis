90f1238b7209364f3781e3a3c542acc760f0d503
Eduardo <eduardoldq1@gmail.com>
2022-01-30 12:11:25 -0300

    chore: add sender id at update validator

--- app/Validators/Review/UpdateValidator.ts
+++ app/Validators/Review/UpdateValidator.ts
@@ -5,6 +5,7 @@ export default class UpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
+    senderId: schema.number([rules.required(), rules.exists({ table: 'users', column: 'id' })]),
     comment: schema.string.optional({}, [rules.minLength(1), rules.maxLength(220)]),
     rating: schema.number.optional([rules.range(1, 5)]),
   })
