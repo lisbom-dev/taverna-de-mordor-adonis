092cea15ef0a3c5dd11b2f653860427177dd2a4e
Eduardo <eduardoldq1@gmail.com>
2022-01-30 21:20:49 -0300

    feat: add pending player controller

---
+++ app/Controllers/Http/PendingPlayersController.ts
@@ -0,0 +1,32 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Board from 'App/Models/Board'
+import StoreValidator from 'App/Validators/BoardPlayer/StoreValidator'
+
+export default class PendingPlayersController {
+  public async store({ request, response, params, bouncer, auth, session }: HttpContextContract) {
+    const board = await Board.find(params.board_id)
+    if (!board) {
+      return response.notFound('Board not found!')
+    }
+    await board.load('pendingPlayers')
+    await bouncer.with('PendingPlayerPolicy').authorize('create', board)
+    const data = await request.validate(StoreValidator)
+    session.flash('success', ['Solicitação enviada com sucesso!'])
+    await board.related('pendingPlayers').attach({
+      [auth.user!.id]: {
+        character_name: data.characterName,
+      },
+    })
+    return response.redirect(`/boards/${board.id}`)
+  }
+
+  public async destroy({ params, response, bouncer, auth }: HttpContextContract) {
+    const board = await Board.find(params.board_id)
+    if (!board) {
+      return response.notFound('Board not found!')
+    }
+    await bouncer.with('PendingPlayerPolicy').authorize('delete', board)
+    board.related('pendingPlayers').detach([auth.user!.id])
+    return response.redirect(`/boards/${board.id}`)
+  }
+}
