8a5e7f1da27446a5286af7e6f767b651cbc2262e
Eduardo <eduardoldq1@gmail.com>
2022-01-30 23:52:39 -0300

    chore: add delete method and modify insert logic

--- app/Controllers/Http/BoardPlayersController.ts
+++ app/Controllers/Http/BoardPlayersController.ts
@@ -1,10 +1,11 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BadRequestException from 'App/Exceptions/BadRequestException'
 import Board from 'App/Models/Board'
+import User from 'App/Models/User'
 import StoreValidator from 'App/Validators/BoardPlayer/StoreValidator'
 
 export default class BoardPlayersController {
-  public async store({ params, response, request, auth, session }: HttpContextContract) {
+  public async store({ params, response, request, bouncer, session }: HttpContextContract) {
     const board = await Board.find(params.board_id)
 
     if (!board) {
@@ -13,17 +14,36 @@ export default class BoardPlayersController {
     if (board.players.length === board.maxPlayers) {
       throw new BadRequestException('All seats have already been filled!')
     }
-
+    const user = await User.find(params.player_id)
+    if (!user) {
+      return response.notFound('User not found!')
+    }
+    await bouncer.with('BoardPlayerPolicy').authorize('create', board, user)
     const data = await request.validate(StoreValidator)
-    session.flash('success', ['${data.characterName} cadastrado a mesa com sucesso!'])
+    session.flash('success', [`${data.characterName} cadastrado a mesa com sucesso!`])
 
     await board.related('players').attach({
-      [auth.user!.id]: {
+      [user.id]: {
         character_name: data.characterName,
         session_who_entered: board.currentSection,
       },
     })
+    await board.related('pendingPlayers').detach([user.id])
 
     return response.redirect('/boards')
+  }
+
+  public async destroy({ params, response, bouncer }: HttpContextContract) {
+    const board = await Board.find(params.board_id)
+    if (!board) {
+      return response.notFound('Board not found!')
+    }
+    const player = board.players.find((p) => p.id === parseInt(params.player_id))
+    if (!player) {
+      return response.notFound('Player not found!')
+    }
+    await bouncer.with('BoardPlayerPolicy').authorize('delete', board, player)
+    await board.related('players').detach([player.id])
+    return response.redirect(`/boards/${board.id}`)
   }
 }
