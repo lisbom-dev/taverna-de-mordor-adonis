b9d5fd4c20a9aa965d97797ae2fb0b7286f44fd7
Eduardo <eduardoldq1@gmail.com>
2022-02-02 20:53:31 -0300

    fix: modify rating range

--- app/Validators/Review/UpdateValidator.ts
+++ app/Validators/Review/UpdateValidator.ts
@@ -7,12 +7,12 @@ export default class UpdateValidator {
   public schema = schema.create({
     senderId: schema.number([rules.required(), rules.exists({ table: 'users', column: 'id' })]),
     comment: schema.string.optional({}, [rules.minLength(1), rules.maxLength(220)]),
-    rating: schema.number.optional([rules.range(1, 5)]),
+    rating: schema.number.optional([rules.range(0.5, 5)]),
   })
 
   public messages = {
     'comment.minLength': 'O comentário não pode estar vazio!',
     'comment.maxLength': 'O comentário não pode ultrapassar o limite de caracteres!',
-    'rating.range': 'Só é possível avaliar entre 1 e 5 estrelas!',
+    'rating.range': 'Só é possível avaliar entre 0,5 e 5 estrelas!',
   }
 }
