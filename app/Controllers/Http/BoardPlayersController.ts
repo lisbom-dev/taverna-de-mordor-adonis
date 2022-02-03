453d26d762fbef5dee0dfa4fd785982033242d9e
Eduardo <eduardoldq1@gmail.com>
2022-02-03 09:20:06 -0300

    chore: remove creation data

--- app/Controllers/Http/BoardPlayersController.ts
+++ app/Controllers/Http/BoardPlayersController.ts
@@ -1,11 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BadRequestException from 'App/Exceptions/BadRequestException'
 import Board from 'App/Models/Board'
-import User from 'App/Models/User'
-import StoreValidator from 'App/Validators/BoardPlayer/StoreValidator'
 
 export default class BoardPlayersController {
-  public async store({ params, response, request, bouncer, session }: HttpContextContract) {
+  public async store({ params, response, bouncer, session }: HttpContextContract) {
     const board = await Board.find(params.board_id)
 
     if (!board) {
@@ -14,21 +12,22 @@ export default class BoardPlayersController {
     if (board.players.length === board.maxPlayers) {
       throw new BadRequestException('All seats have already been filled!')
     }
-    const user = await User.find(params.player_id)
-    if (!user) {
+    const pendingPlayer = board.pendingPlayers.find((p) => p.id === parseInt(params.player_id, 10))
+    if (!pendingPlayer) {
       return response.notFound('User not found!')
     }
-    await bouncer.with('BoardPlayerPolicy').authorize('create', board, user)
-    const data = await request.validate(StoreValidator)
-    session.flash('success', [`${data.characterName} cadastrado a mesa com sucesso!`])
+    await bouncer.with('BoardPlayerPolicy').authorize('create', board, pendingPlayer)
+    session.flash('success', [
+      `${pendingPlayer.$extras.pivot_character_name} cadastrado a mesa com sucesso!`,
+    ])
 
     await board.related('players').attach({
-      [user.id]: {
-        character_name: data.characterName,
+      [pendingPlayer.id]: {
+        character_name: pendingPlayer.$extras.pivot_character_name,
         session_who_entered: board.currentSection,
       },
     })
-    await board.related('pendingPlayers').detach([user.id])
+    await board.related('pendingPlayers').detach([pendingPlayer.id])
 
     return response.redirect('/boards')
   }
