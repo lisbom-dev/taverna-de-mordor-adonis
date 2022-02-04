5f9ed3a4838e8fd792e148e2b500058c117e1753
Eduardo <eduardoldq1@gmail.com>
2022-02-03 23:09:17 -0300

    chore: check if the authenticated user is the master

--- app/Policies/MasterReviewPolicy.ts
+++ app/Policies/MasterReviewPolicy.ts
@@ -3,8 +3,11 @@ import User from 'App/Models/User'
 
 export default class MasterReviewPolicy extends BasePolicy {
   public async create(user: User, master: User) {
-    const masterReview = user.reviews.find((r) => r.senderId === user.id)
+    const masterReview = master.reviews.find((r) => r.senderId === user.id)
     if (masterReview) {
+      return false
+    }
+    if (user.id === master.id) {
       return false
     }
     return true
