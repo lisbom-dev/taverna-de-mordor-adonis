f699c0d815e31eac054d2cd2878d31b5c35eebe1
duardoqueiroz <eduardoldq1@gmail.com>
2022-02-14 19:37:20 -0300

    feat: session's store validator

---
+++ app/Validators/Session/StoreValidator.ts
@@ -0,0 +1,20 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class StoreValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    boardId: schema.number([rules.required(), rules.exists({ column: 'id', table: 'boards' })]),
+    date: schema.date({}, [rules.required(), rules.after(2, 'days')]),
+    time: schema.string({}, [rules.required()]),
+  })
+
+  public messages = {
+    'time.required': 'É necessário informar o horário de início da sessão!',
+    'boardId.required': 'É necessário informar a mesa da sessão!',
+    'boardId.exists': 'A mesa informada não existe!',
+    'date.required': 'É necessário informar a data em que ocorrerá a sessão!',
+    'date.after': 'A sessão precisa ser cadastrada com pelo menos 2 dias de antecedência!',
+  }
+}
