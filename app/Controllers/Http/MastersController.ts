99370ff5d8f09bf00269f169e2f90799b2e73c23
duardoqueiroz <eduardoldq1@gmail.com>
2022-02-24 00:06:28 -0300

    chore: refactor code and add delete method

--- app/Controllers/Http/MastersController.ts
+++ app/Controllers/Http/MastersController.ts
@@ -3,17 +3,31 @@ import BadRequestException from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 
 export default class MastersController {
-  public async store(ctx: HttpContextContract) {
-    const user = await User.find(ctx.params.id)
+  public async store({ params, response, session }: HttpContextContract) {
+    const user = await User.find(params.id)
     if (!user) {
-      return ctx.response.notFound('User not found')
+      return response.notFound('User not found')
     }
     if (user.isMaster) {
       throw new BadRequestException('User already a master!')
     }
     user.isMaster = true
     await user.save()
-    ctx.session.flash('success', ['O usuário ' + user.name + ' Agora é um mestre!'])
-    return ctx.response.redirect('/users')
+    session.flash('success', ['O usuário ' + user.name + ' Agora é um mestre!'])
+    return response.redirect(`/users/${user.id}`)
+  }
+
+  public async delete({ params, response, session }: HttpContextContract) {
+    const user = await User.find(params.id)
+    if (!user) {
+      return response.notFound('User not found')
+    }
+    if (!user.isMaster) {
+      throw new BadRequestException('User is not a master!')
+    }
+    user.isMaster = false
+    await user.save()
+    session.flash('success', ['O usuário ' + user.name + ' Agora é um usuário comum!'])
+    return response.redirect(`/users/${user.id}`)
   }
 }
