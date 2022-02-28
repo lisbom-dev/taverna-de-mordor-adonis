5a1ba9078be4e90bf1e936ac89186f885af69e37
duardoqueiroz <eduardoldq1@gmail.com>
2022-02-28 11:54:43 -0300

    fix: check if board id exists in "event_boards" table

--- app/Validators/Session/StoreValidator.ts
+++ app/Validators/Session/StoreValidator.ts
@@ -5,7 +5,10 @@ export default class StoreValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    boardId: schema.number([rules.required(), rules.exists({ column: 'id', table: 'boards' })]),
+    boardId: schema.number([
+      rules.required(),
+      rules.exists({ column: 'board_id', table: 'event_boards' }),
+    ]),
     date: schema.date({}, [rules.required(), rules.after(2, 'days')]),
     time: schema.string({}, [rules.required()]),
   })
@@ -13,7 +16,7 @@ export default class StoreValidator {
   public messages = {
     'time.required': 'É necessário informar o horário de início da sessão!',
     'boardId.required': 'É necessário informar a mesa da sessão!',
-    'boardId.exists': 'A mesa informada não existe!',
+    'boardId.exists': 'A mesa informada não está inserida em nenhum evento!',
     'date.required': 'É necessário informar a data em que ocorrerá a sessão!',
     'date.after': 'A sessão precisa ser cadastrada com pelo menos 2 dias de antecedência!',
   }
