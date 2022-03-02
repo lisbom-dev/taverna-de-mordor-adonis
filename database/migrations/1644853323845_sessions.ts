4820e9993a6b03b0b2c5fe12dbf50a68c21446c1
duardoqueiroz <eduardoldq1@gmail.com>
2022-03-02 10:28:57 -0300

    chore : transfer values of "times" to "sessions"

--- database/migrations/1644853323845_sessions.ts
+++ database/migrations/1644853323845_sessions.ts
@@ -18,9 +18,65 @@ export default class Sessions extends BaseSchema {
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
+
+    this.defer(async (db) => {
+      const times = await db
+        .from('times')
+        .innerJoin('event_boards', 'event_boards.id', 'times.event_board_id')
+        .innerJoin('events', 'events.id', 'event_boards.event_id')
+
+      await Promise.all(
+        times.map(async (t) => {
+          await db.table('sessions').insert({
+            event_board_id: t.event_board_id,
+            time: t.start_hour,
+            date: t.date,
+            end_hour: t.end_hour,
+            created_at: new Date(),
+            updated_at: new Date(),
+          })
+        })
+      )
+    })
+
+    this.schema.dropTable('times')
   }
 
   public async down() {
+    this.schema.createTable('times', (table) => {
+      table.increments('id')
+      table
+        .integer('event_board_id')
+        .notNullable()
+        .references('id')
+        .inTable('event_boards')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      table.integer('start_hour').notNullable()
+      table.integer('end_hour').notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+
+    this.defer(async (db) => {
+      const sessions = await db
+        .from('sessions')
+        .innerJoin('event_boards', 'event_boards.id', 'sessions.event_board_id')
+        .innerJoin('events', 'events.id', 'event_boards.event_id')
+
+      await Promise.all(
+        sessions.map(async (s) => {
+          await db.table('times').insert({
+            event_board_id: s.event_board_id,
+            start_hour: s.time,
+            end_hour: s.time + 1000,
+            created_at: new Date(),
+            updated_at: new Date(),
+          })
+        })
+      )
+    })
+
     this.schema.dropTable(this.tableName)
   }
 }
