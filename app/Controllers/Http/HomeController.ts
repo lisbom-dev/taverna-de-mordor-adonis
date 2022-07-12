1d731164fa66be9cdd9741c6b7c4a72677f7bdfe
Lucas-Marques-Leao <lucasmarquesleao@outlook.com>
2022-07-12 17:40:52 -0300

    chore: views removal

--- app/Controllers/Http/HomeController.ts
+++
@@ -1,7 +0,0 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class HomeController {
-  public async index({ view }: HttpContextContract) {
-    return view.render('home')
-  }
-}
--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -4,17 +4,17 @@ import User from 'App/Models/User'
 import StoreValidator from 'App/Validators/User/StoreValidator'
 
 export default class UsersController {
-  public async index({ view, request, bouncer }: HttpContextContract) {
+  public async index({ request, bouncer }: HttpContextContract) {
     await bouncer.with('UserPolicy').authorize('invoke')
     const { page = '1' } = request.qs()
     const users = await User.query().paginate(parseInt(page, 10), 10)
-    return view.render('users/list', {
+    return {
       users,
       page,
-    })
+    }
   }
 
-  public async show({ response, view, params, auth, bouncer }: HttpContextContract) {
+  public async show({ response, params, auth, bouncer }: HttpContextContract) {
     const user = await User.find(params.id)
     if (!user) {
       return response.notFound('User not found')
@@ -30,9 +30,7 @@ export default class UsersController {
       await bouncer.with('UserPolicy').authorize('view', user, null)
     }
 
-    return view.render('users/index', {
-      user,
-    })
+    return user
   }
 
   public async store({ request, response, auth, session }: HttpContextContract) {
