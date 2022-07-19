34041c1011f455e6e1e4103902bf87707c90b270
Lucas-Marques-Leao <lucasmarquesleao@outlook.com>
2022-07-18 23:41:54 -0300

    fix: remove session env, and provider

--- env.ts
+++ env.ts
@@ -22,7 +22,6 @@ export default Env.rules({
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   CACHE_VIEWS: Env.schema.boolean(),
-  SESSION_DRIVER: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
   PG_HOST: Env.schema.string({ format: 'host' }),
